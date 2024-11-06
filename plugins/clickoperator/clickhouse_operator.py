d4b4a9a95643ea5263d1eaef8775014d602868f0
Nezox-art <79809240+Nezox-art@users.noreply.github.com>
2024-11-06 17:16:07 +0300

    Create clickhouse_operator.py

---
+++ plugins/clickoperator/clickhouse_operator.py
@@ -0,0 +1,15 @@
+from airflow.models.baseoperator import BaseOperator
+from clickoperator.clickhouse_hook import ClickhouseHook
+
+
+class ClickhouseExecuteOperator(BaseOperator):
+
+
+    def __init__(self, conn_id, sql, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.conn_id = conn_id
+        self.query = sql
+
+    def execute(self, context):
+        hook = ClickhouseHook(self.conn_id)
+        return hook.execute_action(self.query)
