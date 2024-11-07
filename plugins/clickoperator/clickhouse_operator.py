029cb1298a049c1379c41e17df273d8fb2ba81f4
Nezox-art <79809240+Nezox-art@users.noreply.github.com>
2024-11-07 15:48:47 +0300

    Update clickhouse_operator.py

--- plugins/clickoperator/clickhouse_operator.py
+++ plugins/clickoperator/clickhouse_operator.py
@@ -1,5 +1,5 @@
 from airflow.models.baseoperator import BaseOperator
-from plugins.clickoperator.clickhouse_hook import ClickhouseHook
+from clickoperator.clickhouse_hook import ClickhouseHook
 
 
 class ClickhouseExecuteOperator(BaseOperator):
