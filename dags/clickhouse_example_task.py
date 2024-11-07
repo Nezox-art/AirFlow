02699172f9ff7c29e04e6f3399d4073162795239
Nezox-art <79809240+Nezox-art@users.noreply.github.com>
2024-11-07 16:55:19 +0300

    Update clickhouse_example_task.py

--- dags/clickhouse_example_task.py
+++ dags/clickhouse_example_task.py
@@ -3,25 +3,25 @@ from airflow.utils.dates import days_ago
 from airflow_clickhouse_plugin.operators.clickhouse import ClickHouseOperator
 
 default_args = {
-'owner': 'rfathutdinov',
-'catchup': False,
-'description': """
-Пример DAG с подключением к Clickhouse
-"""
+    'owner': 'rfathutdinov',
+    'catchup': False,
+    'description': """
+    Пример DAG с подключением к Clickhouse
+    """
 }
 
-
-with DAG(dag_id='ch_test',
-schedule=None,
-start_date=days_ago(0),
-tags=['clickhouse', 'example'],
-default_args=default_args
+with DAG(
+    dag_id='ch_test',
+    schedule=None,
+    start_date=days_ago(0),
+    tags=['clickhouse', 'example'],
+    default_args=default_args
 ) as dag:
 
-clickhouse_example_task = ClickHouseOperator(
-task_id='clickhouse_example_task',
-sql='SELECT version();',
-clickhouse_conn_id='clickhouse_datago',
-)
+    clickhouse_example_task = ClickHouseOperator(
+        task_id='clickhouse_example_task',
+        sql='SELECT version();',
+        clickhouse_conn_id='clickhouse_datago',
+    )
 
-clickhouse_example_task
+    clickhouse_example_task
